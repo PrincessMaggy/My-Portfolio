9723a7fdef2c4408c3bbcb8698e8649d0c7ef061
PrincessMaggy <edoziemagdalene@gmail.com>
2022-03-29 12:52:15 +0100

    Activated mail

--- mail/contact.php
+++ mail/contact.php
@@ -1,20 +1,37 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();
+// if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+//   http_response_code(500);
+//   exit();
+// }
+
+// $name = strip_tags(htmlspecialchars($_POST['name']));
+// $email = strip_tags(htmlspecialchars($_POST['email']));
+// $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+// $message = strip_tags(htmlspecialchars($_POST['message']));
+
+// $to = "edoziemagdalene5@gmail.com"; 
+// $subject = "$m_subject:  $name";
+// $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+// $header = "From: $email";
+// $header .= "Reply-To: $email";	
+
+// if(!mail($to, $subject, $body, $header))
+//   http_response_code(500);
+
+if(isset($_POST['submit'])){
+  $name = $_POST['name'];
+  $mailFrom = $_POST['mail'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+
+  $mailTo = "edoziemagdalene5@gmail.com"; 
+  $headers = "From: My Website".$mailFrom;
+  $txt = "You have received an email from ".$name.".\n\n".$message;
+  
+  mail($mailTo, $subject, $txt, $headers);
+  header("Location: contact.php?mailsend")
 }
 
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email = strip_tags(htmlspecialchars($_POST['email']));
-$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "edoziemagdalene5@gmail.com"; 
-$subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
 
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
 ?>
