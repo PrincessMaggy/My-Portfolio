c8c01bd8bd3bb762db84cd556c36f93e3c41212c
Maggy <edoziemagdalene@gmail.com>
2023-07-30 23:53:01 +0100

    footer links

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -11,36 +11,68 @@ function Footer() {
                 height={25}
                 alt='img'
             />
-            <Image
-                src={`${basePath}/assets/Github.svg`}
-                width={25}
-                height={25}
-                alt='img'
-            />
-            <Image
-                src={`${basePath}/assets/Twitter.svg`}
-                width={25}
-                height={25}
-                alt='img'
-            />
-            <Image
-                src={`${basePath}/assets/Linkedin.svg`}
-                width={25}
-                height={25}
-                alt='img'
-            />
-            <Image
-                src={`${basePath}/assets/Telegram.svg`}
-                width={25}
-                height={25}
-                alt='img'
-            />
-            <Image
-                src={`${basePath}/assets/Email.svg`}
-                width={25}
-                height={25}
-                alt='img'
-            />
+            <a
+                href='https://github.com/PrincessMaggy'
+                target='_blank'
+                rel='noopener noreferrer'
+            >
+                <Image
+                    src={`${basePath}/assets/Github.svg`}
+                    width={25}
+                    height={25}
+                    alt='img'
+                    href='https://github.com/PrincessMaggy'
+                />
+            </a>
+            <a
+                href='https://twitter.com/home'
+                target='_blank'
+                rel='noopener noreferrer'
+            >
+                <Image
+                    src={`${basePath}/assets/Twitter.svg`}
+                    width={25}
+                    height={25}
+                    alt='img'
+                    href='https://twitter.com/home'
+                />
+            </a>
+            <a
+                href='https://www.linkedin.com/in/edoziemagdalene/'
+                target='_blank'
+                rel='noopener noreferrer'
+            >
+                <Image
+                    src={`${basePath}/assets/Linkedin.svg`}
+                    width={25}
+                    height={25}
+                    alt='img'
+                />
+            </a>
+            <a
+                href='https://t.me/princessmaggy'
+                target='_blank'
+                rel='noopener noreferrer'
+            >
+                <Image
+                    src={`${basePath}/assets/Telegram.svg`}
+                    width={25}
+                    height={25}
+                    alt='img'
+                />
+            </a>
+            <a
+                href='mailto:edoziemagdalene@gmail.com'
+                target='_blank'
+                rel='noopener noreferrer'
+            >
+                <Image
+                    src={`${basePath}/assets/Email.svg`}
+                    width={25}
+                    height={25}
+                    alt='img'
+                />
+            </a>{' '}
         </div>
     );
 }
