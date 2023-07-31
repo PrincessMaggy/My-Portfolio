095de66aea4f5d620d7345d4be575ca90c4376ae
Maggy <edoziemagdalene@gmail.com>
2023-07-31 16:08:26 +0100

    portfolio page

---
+++ components/Card/Card.js
@@ -0,0 +1,304 @@
+import React from 'react';
+import {motion} from 'framer-motion';
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from '../../src/styles/Portfolio.module.scss';
+import {useRouter} from 'next/router';
+
+function Card() {
+    const router = useRouter();
+    const basePath = router.basePath || '';
+
+    const item = {
+        hidden: {opacity: 0, y: 20},
+        show: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                ease: 'easeInOut',
+                duration: 0.2,
+            },
+        },
+    };
+    return (
+        <>
+            <motion.div className={styles.card} variants={item}>
+                <motion.div>
+                    <Link
+                        href='https://movie-app-eight-rust.vercel.app'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <motion.button
+                            whileHover={{
+                                scale: 1.1,
+                                transition: {duration: 0.3},
+                            }}
+                            whileTap={{scale: 0.9}}
+                            transition={{
+                                type: 'spring',
+                                stiffness: 400,
+                                damping: 10,
+                            }}
+                        >
+                            WatchFlix
+                        </motion.button>
+                    </Link>
+                </motion.div>
+                <motion.div
+                    whileHover={{scale: 1.1}}
+                    transition={{
+                        type: 'spring',
+                        stiffness: 400,
+                        damping: 10,
+                    }}
+                >
+                    <Link
+                        href='https://movie-app-eight-rust.vercel.app'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <Image
+                            src={`${basePath}/assets/projects (1).png`}
+                            alt='project'
+                            width={300}
+                            height={200}
+                        />
+                    </Link>
+                </motion.div>
+            </motion.div>
+
+            <motion.div className={styles.card} variants={item}>
+                <motion.div>
+                    <Link
+                        href='https://coffee-app-sandy.vercel.app/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <motion.button
+                            whileHover={{
+                                scale: 1.1,
+                                transition: {duration: 0.3},
+                            }}
+                            whileTap={{scale: 0.9}}
+                            transition={{
+                                type: 'spring',
+                                stiffness: 400,
+                                damping: 10,
+                            }}
+                        >
+                            Coffee app
+                        </motion.button>
+                    </Link>
+                </motion.div>
+                <motion.div
+                    whileHover={{scale: 1.1}}
+                    transition={{
+                        type: 'spring',
+                        stiffness: 400,
+                        damping: 10,
+                    }}
+                >
+                    <Link
+                        href='https://coffee-app-sandy.vercel.app/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <Image
+                            src={`${basePath}/assets/projects (2).png`}
+                            alt='project'
+                            width={300}
+                            height={200}
+                        />
+                    </Link>
+                </motion.div>
+            </motion.div>
+
+            <motion.div className={styles.card} variants={item}>
+                <motion.div>
+                    <Link
+                        href='https://princessmaggy.github.io/github_finder/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <motion.button
+                            whileHover={{
+                                scale: 1.1,
+                                transition: {duration: 0.3},
+                            }}
+                            whileTap={{scale: 0.9}}
+                            transition={{
+                                type: 'spring',
+                                stiffness: 400,
+                                damping: 10,
+                            }}
+                        >
+                            Github Finder
+                        </motion.button>
+                    </Link>
+                </motion.div>
+                <motion.div
+                    whileHover={{scale: 1.1}}
+                    transition={{
+                        type: 'spring',
+                        stiffness: 400,
+                        damping: 10,
+                    }}
+                >
+                    <Link
+                        href='https://princessmaggy.github.io/github_finder/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <Image
+                            src={`${basePath}/assets/projects (1).svg`}
+                            alt='project'
+                            width={300}
+                            height={200}
+                        />
+                    </Link>
+                </motion.div>
+            </motion.div>
+
+            <motion.div className={styles.card} variants={item}>
+                <motion.div>
+                    <Link
+                        href='https://princessmaggy.github.io/blogr/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <motion.button
+                            whileHover={{
+                                scale: 1.1,
+                                transition: {duration: 0.3},
+                            }}
+                            whileTap={{scale: 0.9}}
+                            transition={{
+                                type: 'spring',
+                                stiffness: 400,
+                                damping: 10,
+                            }}
+                        >
+                            Blog
+                        </motion.button>
+                    </Link>
+                </motion.div>
+                <motion.div
+                    whileHover={{scale: 1.1}}
+                    transition={{
+                        type: 'spring',
+                        stiffness: 400,
+                        damping: 10,
+                    }}
+                >
+                    <Link
+                        href='https://princessmaggy.github.io/blogr/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <Image
+                            src={`${basePath}/assets/projects (2).svg`}
+                            alt='project'
+                            width={300}
+                            height={200}
+                        />
+                    </Link>
+                </motion.div>
+            </motion.div>
+
+            <motion.div className={styles.card} variants={item}>
+                <motion.div>
+                    <Link
+                        href='https://princessmaggy.github.io/expense-tracker/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <motion.button
+                            whileHover={{
+                                scale: 1.1,
+                                transition: {duration: 0.3},
+                            }}
+                            whileTap={{scale: 0.9}}
+                            transition={{
+                                type: 'spring',
+                                stiffness: 400,
+                                damping: 10,
+                            }}
+                        >
+                            Expense Tracker
+                        </motion.button>
+                    </Link>
+                </motion.div>
+                <motion.div
+                    whileHover={{scale: 1.1}}
+                    transition={{
+                        type: 'spring',
+                        stiffness: 400,
+                        damping: 10,
+                    }}
+                >
+                    <Link
+                        href='https://princessmaggy.github.io/expense-tracker/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <Image
+                            src={`${basePath}/assets/projects (3).svg`}
+                            alt='project'
+                            width={300}
+                            height={200}
+                        />
+                    </Link>
+                </motion.div>
+            </motion.div>
+
+            <motion.div className={styles.card} variants={item}>
+                <motion.div>
+                    <Link
+                        href='https://princessmaggy.github.io/music-player/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <motion.button
+                            whileHover={{
+                                scale: 1.1,
+                                transition: {duration: 0.3},
+                            }}
+                            whileTap={{scale: 0.9}}
+                            transition={{
+                                type: 'spring',
+                                stiffness: 400,
+                                damping: 10,
+                            }}
+                        >
+                            Music Player
+                        </motion.button>
+                    </Link>
+                </motion.div>
+                <motion.div
+                    whileHover={{scale: 1.1}}
+                    transition={{
+                        type: 'spring',
+                        stiffness: 400,
+                        damping: 10,
+                    }}
+                >
+                    <Link
+                        href='https://princessmaggy.github.io/music-player/'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <Image
+                            src={`${basePath}/assets/projects (4).svg`}
+                            alt='project'
+                            width={300}
+                            height={200}
+                        />
+                    </Link>
+                </motion.div>
+            </motion.div>
+        </>
+    );
+}
+
+export default Card;
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,20 +3,99 @@ import React from 'react';
 import Nav from '../../components/Nav/Nav';
 import Footer from '../../components/Footer/Footer';
 import styles from '../styles/Portfolio.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+import {motion} from 'framer-motion';
+import Head from 'next/head';
+import Card from '../../components/Card/Card';
 
 function Portfolio() {
     const router = useRouter();
     const basePath = router.basePath || '';
     const rootStyles = `url(${basePath}/assets/Background.png), #323540 50% / cover no-repeat`;
+
+    let easing = [0.6, -0.05, 0.01, 0.99];
+
+    const container = {
+        show: {
+            transition: {
+                staggerChildren: 0.2,
+            },
+        },
+    };
+
+    const item = {
+        hidden: {opacity: 0, y: 20},
+        show: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                ease: 'easeInOut',
+                duration: 0.2,
+            },
+        },
+    };
+
+    const title = {
+        hidden: {
+            y: 60,
+            opacity: 0,
+        },
+        show: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                delay: 0.2,
+                duration: 0.6,
+                ease: easing,
+            },
+        },
+    };
+
+    const hoverEffect = {
+        whileHover: {
+            scale: 1.5,
+            rotate: 630,
+            borderRadius: '100%',
+        },
+        whileTap: {
+            scale: 0.8,
+            rotate: 630,
+            borderRadius: '100%',
+        },
+    };
+
     return (
         <div>
+            <Head>
+                <title> Portfolio </title>
+                <meta
+                    name='description'
+                    content='Portfolio - Edozie Magdalene'
+                />
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1'
+                />
+                <link rel='icon' href={`${basePath}/assets/Logo.png`} />
+            </Head>
             <Nav />
             <div
                 className={styles.portfolioMain}
                 style={{background: rootStyles}}
             >
-                {' '}
-                portfolio
+                <motion.header variants={title}>
+                    <motion.h3>My Projects</motion.h3>
+                </motion.header>
+                <motion.main
+                    variants={container}
+                    initial='hidden'
+                    exit='exit'
+                    whileInView='show'
+                    viewport={{once: false}}
+                >
+                    <Card />
+                </motion.main>
             </div>
             <Footer />
         </div>
