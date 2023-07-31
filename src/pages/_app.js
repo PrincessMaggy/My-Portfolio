668daa8986e2818286e44e6cecfb551aa44e2b46
Maggy <edoziemagdalene@gmail.com>
2023-07-31 01:19:34 +0100

    fix scroll to top issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
-import '@/styles/globals.css'
+// _app.js
+import React from 'react';
+import '@/styles/globals.css';
+import {useRouter} from 'next/router'; // Import useRouter from next/router
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}) {
+    const router = useRouter(); // Get the router object
+    React.useEffect(() => {
+        const handleRouteChange = () => {
+            window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+        };
+
+        // Add the event listener for route changes
+        router.events.on('routeChangeComplete', handleRouteChange);
+
+        // Remove the event listener when the component is unmounted
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, [router.events]);
+
+    return <Component {...pageProps} />;
 }
