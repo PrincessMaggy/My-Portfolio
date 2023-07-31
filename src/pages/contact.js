72c7a64187748795dc73fb629763c0eca5a3a2ac
Maggy <edoziemagdalene@gmail.com>
2023-07-31 18:33:11 +0100

    contactpages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,6 +3,8 @@ import React from 'react';
 import Nav from '../../components/Nav/Nav';
 import Footer from '../../components/Footer/Footer';
 import styles from '../styles/Contact.module.scss';
+import Head from 'next/head';
+import Image from 'next/image';
 
 function Contact() {
     const router = useRouter();
@@ -11,12 +13,76 @@ function Contact() {
 
     return (
         <div>
+            <Head>
+                <title> Contact </title>
+                <meta name='description' content='Contact - Edozie Magdalene' />
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1'
+                />
+                <link rel='icon' href={`${basePath}/assets/Logo.png`} />
+            </Head>
             <Nav />
             <div
                 className={styles.contactMain}
                 style={{background: rootStyles}}
             >
-                contact
+                <section className={styles.left}>
+                    <header>
+                        <div>
+                            <Image
+                                src={`${basePath}/assets/Logo.png`}
+                                alt='img'
+                                width={70}
+                                height={70}
+                            />
+                        </div>
+                        <h3>Get in touch — Let’s start a journey together</h3>
+                    </header>
+                    <form>
+                        <div>
+                            <label htmlFor='name'>Name</label>
+                            <input type='text' id='name' required />
+                        </div>
+                        <div>
+                            <label htmlFor='email'>Email</label>
+                            <input type='email' id='email' required />
+                        </div>
+                        <div>
+                            <label htmlFor='phone'>Phone Number</label>
+                            <input type='tel' id='phone' required />
+                        </div>
+                        <div>
+                            <label htmlFor='msg'>Message</label>
+                            <textarea
+                                id='msg'
+                                rows={10}
+                                cols={30}
+                                required
+                            ></textarea>
+                        </div>
+                        <div className={styles.formFooter}>
+                            <div className={styles.captcha}>
+                                <input type='checkbox' />
+                                <span>I am not a robot</span>
+                                <Image
+                                    src={`${basePath}/assets/captcha.png`}
+                                    width={20}
+                                    height={20}
+                                    alt='captcha'
+                                />
+                            </div>
+                            <div>
+                                <button type='submit'>Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                    <p>
+                        By submitting this form I accept the Privacy Policy of
+                        this site.
+                    </p>
+                </section>
+                <section className={styles.map}>{/* map */}</section>
             </div>
             <Footer />
         </div>
